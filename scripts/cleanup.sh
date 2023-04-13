a239784271a54ab2094edcbb7337ced755e911bd
Roger Amorim <rogertc16@gmail.com>
2023-04-13 20:34:16 -0300

    chore: remove sudo from docker commands

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,5 +4,5 @@
 rm -rf gaia-source-code
 
 # stop and remove docker image
-sudo docker stop $(sudo docker ps -q --filter ancestor=gaia && sudo docker ps -q --filter ancestor=gaia-db)
-sudo docker rmi $(sudo docker images "gaia-db" -a -q) -f && sudo docker rmi $(sudo docker images "gaia" -a -q) -f
+docker stop $(docker ps -q --filter ancestor=gaia && docker ps -q --filter ancestor=gaia-db)
+docker rmi $(docker images "gaia-db" -a -q) -f && docker rmi $(docker images "gaia" -a -q) -f
